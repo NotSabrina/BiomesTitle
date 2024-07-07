3eaf2b6c908af25934e8a07990ff5feea8b93803
NotSabrina <126478510+NotSabrina@users.noreply.github.com>
2024-07-07 21:47:09 +0700

    Add files via upload
    1.1-A

--- src/main/java/me/supramental/biomestitle/BiomesTitle.java
+++ src/main/java/me/supramental/biomestitle/BiomesTitle.java
@@ -1,18 +1,21 @@
 package me.supramental.biomestitle;
 
-import me.supramental.biomestitle.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.clip.placeholderapi.*;
-import me.clip.placeholderapi.PlaceholderHook;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class BiomesTitle extends JavaPlugin {
 
     int serviceId = 22389;
     Metrics metrics = new Metrics(this, serviceId);
     private boolean isPlaceholderAPIAvailable = false;
+    private static final String VERSION_URL = "https://raw.githubusercontent.com/NotSabrina/BiomesTitle/main/version.txt";
 
     @Override
     public void onEnable() {
@@ -28,6 +31,8 @@ public class BiomesTitle extends JavaPlugin {
 
         Bukkit.getPluginManager().registerEvents(new BiomeListener(this, isPlaceholderAPIAvailable), this);
         getLogger().info("BiomesTitle has been enabled!");
+
+        checkForUpdates();
     }
 
     @Override
@@ -37,5 +42,36 @@ public class BiomesTitle extends JavaPlugin {
 
     public boolean isPlaceholderAPIAvailable() {
         return isPlaceholderAPIAvailable;
+    }
+
+    private void checkForUpdates() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL(VERSION_URL).openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(5000);
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String latestVersion = in.readLine();
+                    in.close();
+
+                    String currentVersion = getDescription().getVersion();
+                    if (!currentVersion.equals(latestVersion)) {
+                        notifyAdmins(latestVersion);
+                    }
+
+                } catch (Exception e) {
+                    getLogger().severe("Failed to check for updates: " + e.getMessage());
+                }
+            }
+        }.runTaskTimerAsynchronously(this, 0, 20 * 60 * 60);
+    }
+
+    private void notifyAdmins(String latestVersion) {
+        String message = "A new version of BiomesTitle is available: " + latestVersion + ". You are using version: " + getDescription().getVersion();
+        getLogger().info(message);
     }
 }
