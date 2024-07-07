0253d146f9e0c3ee931fb98488844df3f56d5075
NotSabrina <126478510+NotSabrina@users.noreply.github.com>
2024-07-07 21:11:35 +0700

    Add files via upload
    1.1 update

--- src/main/java/me/supramental/biomestitle/BiomeListener.java
+++ src/main/java/me/supramental/biomestitle/BiomeListener.java
@@ -1,6 +1,8 @@
 package me.supramental.biomestitle;
 
-import org.bukkit.block.Biome;
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -9,12 +11,16 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class BiomeListener implements Listener {
 
     private final JavaPlugin plugin;
+    private final boolean isPlaceholderAPIAvailable;
 
-    public BiomeListener(JavaPlugin plugin) {
+    public BiomeListener(JavaPlugin plugin, boolean isPlaceholderAPIAvailable) {
         this.plugin = plugin;
+        this.isPlaceholderAPIAvailable = isPlaceholderAPIAvailable;
     }
 
     @EventHandler
@@ -23,12 +29,40 @@ public class BiomeListener implements Listener {
         Location from = event.getFrom();
         Location to = event.getTo();
 
-        if (to != null && from.getBlock().getBiome() != to.getBlock().getBiome()) {
-            Biome newBiome = to.getBlock().getBiome();
-            String title = plugin.getConfig().getString("biomes." + newBiome.name());
+        if (to == null) {
+            return;
+        }
 
-            if (title != null) {
-                player.sendTitle(ChatColor.translateAlternateColorCodes('&', title), "", 10, 70, 20);
+        List<String> disabledWorlds = plugin.getConfig().getStringList("disabled-worlds");
+        if (disabledWorlds.contains(player.getWorld().getName())) {
+            return;
+        }
+
+        if (from.getBlock().getBiome() != to.getBlock().getBiome()) {
+            org.bukkit.block.Biome newBiome = to.getBlock().getBiome();
+            displayBiomeMessage(player, newBiome.name());
+        }
+    }
+
+    private void displayBiomeMessage(Player player, String biomeName) {
+        String path = "biomes." + biomeName;
+        String message = plugin.getConfig().getString(path + ".message");
+        String displayType = plugin.getConfig().getString(path + ".display");
+
+        if (message != null && displayType != null) {
+            if (isPlaceholderAPIAvailable) {
+                message = PlaceholderAPI.setPlaceholders(player, message);
+            }
+            String formattedMessage = ChatColor.translateAlternateColorCodes('&', message);
+            switch (displayType.toLowerCase()) {
+                case "title":
+                    player.sendTitle(formattedMessage, "", 10, 70, 20);
+                    break;
+                case "actionbar":
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(formattedMessage));
+                    break;
+                default:
+                    plugin.getLogger().warning("Invalid display type for biome " + biomeName + ": " + displayType);
             }
         }
     }
--- src/main/java/me/supramental/biomestitle/BiomesTitle.java
+++ src/main/java/me/supramental/biomestitle/BiomesTitle.java
@@ -2,23 +2,40 @@ package me.supramental.biomestitle;
 
 import me.supramental.biomestitle.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import me.clip.placeholderapi.PlaceholderAPI;
+import me.clip.placeholderapi.*;
+import me.clip.placeholderapi.PlaceholderHook;
 
 public class BiomesTitle extends JavaPlugin {
 
     int serviceId = 22389;
     Metrics metrics = new Metrics(this, serviceId);
+    private boolean isPlaceholderAPIAvailable = false;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        Bukkit.getPluginManager().registerEvents(new BiomeListener(this), this);
-        getLogger().info("BiomesTitle has been enabled!");
 
+        Plugin placeholderapiPlugin = getServer().getPluginManager().getPlugin("PlaceholderAPI");
+        if (placeholderapiPlugin != null && placeholderapiPlugin.isEnabled()) {
+            isPlaceholderAPIAvailable = true;
+            getLogger().info("PlaceholderAPI detected. Enabling PlaceholderAPI support.");
+        } else {
+            getLogger().info("PlaceholderAPI not found. Running without PlaceholderAPI support.");
+        }
+
+        Bukkit.getPluginManager().registerEvents(new BiomeListener(this, isPlaceholderAPIAvailable), this);
+        getLogger().info("BiomesTitle has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("BiomesTitle has been disabled!");
+    }
+
+    public boolean isPlaceholderAPIAvailable() {
+        return isPlaceholderAPIAvailable;
     }
 }
